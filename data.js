608661c1d4790f21ea323ee3fa1adef814d2e9d2
catharygr <103951447+catharygr@users.noreply.github.com>
2023-05-13 22:22:34 +0100

    Trabajando en App y css

---
+++ data.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-undef */
+export default QUOTES = [
+  {
+    id: 1,
+    color: "red",
+    author: "-Colina de napoleón",
+    quote:
+      "Cualquier cosa que la mente del hombre pueda concebir y creer, puede lograrlo.",
+  },
+  {
+    id: 2,
+    color: "red",
+    author: "-Bob maya angelou",
+    quote: "No se puede agotar la creatividad. Cuanto más usas, más tienes..",
+  },
+  {
+    id: 3,
+    color: "red",
+    author: "-Bob normando vicent peale",
+    quote: "Cambia tus pensamientos y cambiarás tu mundo.",
+  },
+  {
+    id: 4,
+    color: "red",
+    author: "-Bob Confucio",
+    quote: "Todo tiene belleza, pero no todos pueden ver.",
+  },
+  {
+    id: 5,
+    color: "red",
+    author: "-Bob gloria steinem",
+    quote: "Soñar, después de todo, es una forma de planificación.",
+  },
+  {
+    id: 6,
+    color: "purple",
+    author: "-Bob Henry David Thoreau",
+    quote:
+      "Vaya con confianza en dirección de sus sueños. Vive la vida que has imaginado",
+  },
+  {
+    id: 7,
+    color: "purple",
+    author: "-Bob Booker T.Washington",
+    quote: "Si quieres levantarte a ti mismo, levanta a alguien más.",
+  },
+  {
+    id: 8,
+    color: "purple",
+    author: "-Bob normando vicent peale",
+    quote: "Cambia tus pensamientos y cambiarás tu mundo.",
+  },
+  {
+    id: 9,
+    color: "purple",
+    author: "-Bob Pablo Picasso",
+    quote:
+      "Todo niño es un artista. El problema es cómo seguir siendo un artista una vez que crece.",
+  },
+  {
+    id: 10,
+    color: "purple",
+    author: "-Bob mae jemison",
+    quote:
+      "Es tu lugar en el mundo; es tu vida. Continúa y haz todo lo que puedas con él, y conviértelo en la vida que quieres vivir.",
+  },
+];
